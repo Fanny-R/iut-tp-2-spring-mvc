3306a8bf9e74ca2c14ce144e35917b69efb49475
Fanny-R <fanny.rottee@gmail.com>
2017-05-03 09:41:36 +0200

    correction de l'étape 2

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,12 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class RootController {
 
   @RequestMapping("/")
-  public ModelAndView test(HttpServletRequest request) {
+  public ModelAndView test(HttpServletRequest request, @RequestHeader HttpHeaders headers) {
     ModelAndView mav = new ModelAndView();
     mav.addObject("titre", "IUT");
     mav.addObject("corps", "bonjour");
 
-    List<String> headerList = new ArrayList<String>(Collections.list(request.getHeaderNames()));
+
+    List<String> headerList = new ArrayList<String>(headers.keySet());
 
     mav.addObject("header", headerList);
 
